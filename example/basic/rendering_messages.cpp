c27e9de6ff0ead67686df392b698d640239d065d
Borislav Stanimirov <b.stanimirov@abv.bg>
2019-12-07 17:42:54 +0200

    Copyright years

--- example/basic/rendering_messages.cpp
+++ example/basic/rendering_messages.cpp
@@ -1,5 +1,5 @@
 // DynaMix
-// Copyright (c) 2013-2016 Borislav Stanimirov, Zahary Karadjov
+// Copyright (c) 2013-2019 Borislav Stanimirov, Zahary Karadjov
 //
 // Distributed under the MIT Software License
 // See accompanying file LICENSE.txt or copy at
