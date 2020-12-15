251b0403ec581d39f4eebdf23d770f594342ae23
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-15 16:44:42 +0200

    Removed outdated generator scripts

--- tools/gen_vs14.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist gen mkdir gen
-cd gen
-if not exist vs14x64 mkdir vs14x64
-cd vs14x64
-cmake ../../.. -DDYNAMIX_BUILD_PERF=1 -G "Visual Studio 14 2015 Win64"
-cd ..\..
--- tools/gen_vs15.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist gen mkdir gen
-cd gen
-if not exist vs15x64 mkdir vs15x64
-cd vs15x64
-cmake ../../.. -DDYNAMIX_BUILD_PERF=1 -G "Visual Studio 15 2017 Win64"
-cd ..\..
