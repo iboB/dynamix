5d0c02b4de76195641472d946454e116dc541cd4
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-03-17 16:58:42 +0200

    pmr compat: fixed condition for DYNAMIX_HAS_PMR

--- code/dynamix/compat/pmr/pmr.hpp
+++ code/dynamix/compat/pmr/pmr.hpp
@@ -6,8 +6,8 @@
 
 #define DYNAMIX_FORCE_PMR_COMPAT 0
 
-#if !DYNAMIX_FORCE_PMR_COMPAT || (defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 16000)
-#   define DYNAMIX_HAS_PMR 1
-#else
+#if DYNAMIX_FORCE_PMR_COMPAT || (defined(_LIBCPP_VERSION) && _LIBCPP_VERSION < 16000)
 #   define DYNAMIX_HAS_PMR 0
+#else
+#   define DYNAMIX_HAS_PMR 1
 #endif
