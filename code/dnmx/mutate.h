7f677a88844d04fc133a9683eb1f4515e979e519
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-03-23 07:50:38 +0200

    tests, capi: mutate, ref #61

--- code/dnmx/mutate.h
+++ code/dnmx/mutate.h
@@ -17,9 +17,10 @@ typedef struct dnmx_mixin_info dnmx_mixin_info;
 typedef dnmx_error_return_t(*dnmx_mutate_func)(const dnmx_mixin_info* info, uintptr_t user_data, dnmx_mixin_index_t index, void* mixin);
 
 typedef struct dnmx_mutate_to_override {
-    const dnmx_mixin_info* mixin;
-    dnmx_mutate_func init_new;
-    dnmx_mutate_func update_common;
+    const dnmx_mixin_info* mixin; // can be null in which case mixin_name is checked
+    dnmx_sv mixin_name; // will only be checked in mixin is null
+    dnmx_mutate_func init_new; // called for new mixins (only in new type)
+    dnmx_mutate_func update_common; // called for common mixins (in new_type and old object type)
     uintptr_t user_data; // user data for this override
 } dnmx_mutate_to_override;
 DYNAMIX_API dnmx_error_return_t dnmx_mutate_to(dnmx_object_handle obj, dnmx_type_handle type, const dnmx_mutate_to_override* overrides, uint32_t num_overrides);
