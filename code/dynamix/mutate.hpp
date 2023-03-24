907b37906872a3d7f3a19cf2ddcb32f763087b2a
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-03-24 06:46:25 +0200

    mutations: gradiual mutations wip, ref #69

--- code/dynamix/mutate.hpp
+++ code/dynamix/mutate.hpp
@@ -6,6 +6,7 @@
 #include "mutate_to.hpp"
 
 namespace dynamix {
+// regular mutate with ops as arguments
 template <typename... Ops>
 void mutate(object& obj, Ops&&... ops) {
     type_mutation type_mut(obj.get_type());
@@ -15,5 +16,28 @@ void mutate(object& obj, Ops&&... ops) {
     auto& type = obj.get_domain().get_type(std::move(type_mut));
 
     mutate_to(obj, type, std::forward<Ops>(ops)...);
+}
+
+// v1 comaptible mutate which only works with mixins with default init functions
+// not extensible
+class gradual_mutation {
+    type_mutation m_type_mut;
+    object& m_obj;
+public:
+    gradual_mutation(object& obj)
+        : m_type_mut(obj.get_type())
+        , m_obj(obj)
+    {}
+
+    using self_t = gradual_mutation;
+
+    template <typename Mixin>
+    self_t& add() {
+        m_type_mut.add<Mixin>();
+        return *this;
+    }
+};
+gradual_mutation mutate(object& obj) {
+    return gradual_mutation(obj);
 }
 }
