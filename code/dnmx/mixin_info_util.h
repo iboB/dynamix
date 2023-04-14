1840013f8cc37929ade54aa2a41033a038f1b16b
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-04-14 13:10:10 +0300

    example: c hello world

--- code/dnmx/mixin_info_util.h
+++ code/dnmx/mixin_info_util.h
@@ -24,3 +24,14 @@
     (mi).copy_init = dnmx_mixin_common_copy_func; \
     (mi).copy_asgn = dnmx_mixin_common_copy_func; \
 } while (false)
+
+#define DNMX_COMMON_INIT_EMPTY_MI(mi, mixin_name) do { \
+    (mi) = dnmx_make_mixin_info(); \
+    (mi).name = dnmx_make_sv_lit(mixin_name); \
+    (mi).init = dnmx_mixin_noop_init_func; \
+    (mi).move_init = dnmx_mixin_noop_move_func; \
+    (mi).move_asgn = dnmx_mixin_noop_move_func; \
+    (mi).copy_init = dnmx_mixin_noop_copy_func; \
+    (mi).copy_asgn = dnmx_mixin_noop_copy_func; \
+    (mi).compare = dnmx_mixin_noop_cmp_func; \
+} while (false)
---
+++ example/hello-c-world/hello-world.c
@@ -0,0 +1,100 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#include <dnmx/domain.h>
+#include <dnmx/feature_info.h>
+#include <dnmx/mixin_info.h>
+#include <dnmx/object.h>
+#include <dnmx/type.h>
+#include <dnmx/mutate.h>
+#include <dnmx/mixin_info_util.h>
+
+#include <stdio.h>
+
+struct hello_domain_def {
+    dnmx_domain_handle domain;
+    dnmx_feature_info speak;
+};
+
+struct hello_domain_def hello_domain;
+
+dnmx_mixin_info hello;
+dnmx_mixin_info bye;
+
+void speak(dnmx_object_handle obj);
+
+void init_hello_domain(void);
+void destroy_hello_domain(void);
+void init_hello_mixins(void);
+
+int main() {
+    init_hello_domain();
+    init_hello_mixins();
+    dnmx_object_handle greeter = dnmx_create_object_empty(hello_domain.domain);
+
+    {
+        dnmx_mutate_op op = {.op_type = dnmx_mutate_op_add, .mixin = &hello};
+        dnmx_mutate(greeter, &op, 1);
+    }
+
+    speak(greeter);
+
+    {
+        dnmx_mutate_op ops[] = {
+            {.op_type = dnmx_mutate_op_remove, .mixin = &hello},
+            {.op_type = dnmx_mutate_op_add, .mixin = &bye}
+        };
+        dnmx_mutate(greeter, ops, 2);
+    }
+
+    speak(greeter);
+
+    dnmx_destroy_object(greeter);
+    destroy_hello_domain();
+    return 0;
+}
+
+void init_hello_domain(void) {
+    hello_domain.domain = dnmx_create_domain(dnmx_make_sv_lit("example"), (dnmx_domain_settings){0}, 0, NULL);
+    hello_domain.speak = dnmx_make_feature_info();
+    hello_domain.speak.name = dnmx_make_sv_lit("speak");
+}
+
+void say_hello(void) {
+    puts("Hello from DynaMix!");
+}
+dnmx_feature_for_mixin hello_feature_for_mixin = {
+    .info = &hello_domain.speak,
+    .payload = (void*)(say_hello),
+};
+
+void say_bye(void) {
+    puts("Good bye from DynaMix.");
+}
+dnmx_feature_for_mixin bye_feature_for_mixin = {
+    .info = &hello_domain.speak,
+    .payload = (void*)(say_bye),
+};
+
+void init_hello_mixins(void) {
+    DNMX_COMMON_INIT_EMPTY_MI(hello, "hello");
+    hello.features = &hello_feature_for_mixin;
+    hello.num_features = 1;
+    dnmx_register_mixin(hello_domain.domain, &hello);
+    DNMX_COMMON_INIT_EMPTY_MI(bye, "bye");
+    bye.features = &bye_feature_for_mixin;
+    bye.num_features = 1;
+    dnmx_register_mixin(hello_domain.domain, &bye);
+}
+
+void destroy_hello_domain(void) {
+    dnmx_destroy_domain(hello_domain.domain);
+}
+
+typedef void (*vv_func)(void);
+
+void speak(dnmx_object_handle obj) {
+    dnmx_ftable_entry e = dnmx_ftable_at(obj->m_type, hello_domain.speak.id);
+    vv_func func = (vv_func)e.begin->payload;
+    func();
+}
