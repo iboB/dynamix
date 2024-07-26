761c95c29dd8a7b189e489d0731c9bc2e6e8bfa5
Borislav Stanimirov <b.stanimirov@abv.bg>
2024-07-26 15:01:03 +0300

    msg macros: make use of splat's new meta selector

--- code/dynamix/msg/msg_macros.hpp
+++ code/dynamix/msg/msg_macros.hpp
@@ -8,4 +8,4 @@
 #define DYNAMIX_FUNC_TRAITS_NAME(func_name) I_DNMX_PP_CAT(_dynamix_traits_for_, func_name)
 
 #define I_DNMX_FWD_ARG(t, i) std::forward<t>(I_DNMX_PP_CAT(a, i))
-#define I_DNMX_FWD_ARGS(...) I_DNMX_ITERATE_BY(I_DNMX_FWD_ARG, ##__VA_ARGS__)
+#define I_DNMX_FWD_ARGS(...) SPLAT_ITERATE_WITH(I_DNMX_FWD_ARG, ##__VA_ARGS__)
